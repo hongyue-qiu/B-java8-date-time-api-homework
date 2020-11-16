824e11571338ae4099432e10bc7b3ed928dadda1
hongyue-qiu <994059641@qq.com>
2020-11-16 22:19:10 +0800

    finished practice2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
 
 /**
  * 对任意日期获取下一个工作日, 不考虑节假日
@@ -11,6 +15,9 @@ import java.time.LocalDate;
 public class Practice2 {
 
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    if (date.getDayOfWeek().equals(DayOfWeek.FRIDAY)) {
+      return date.plusDays(3);
+    }
+    return date.plusDays(1);
   }
 }
